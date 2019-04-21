a21dac92c59ada0c97a45d19eeb84ebb2bec5d11
Galen Casstevens <Galen.Casstevens@agdata.com>
2019-04-21 11:17:29 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPractice
+{
+    public static class Program
+    {
+        [ThreadStatic]
+        public static int _field;
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread A: {0}", _field);
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread B: {0}", _field);
+                }
+            }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
